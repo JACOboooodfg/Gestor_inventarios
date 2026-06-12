b58e7231a6722bc04e751edca82ba3cc32313f91
JACOboooodfg <jacobsenasofia@gmail.com>
2026-06-12 10:22:52 -0500

    mensaje

--- inventory/chatbot_views.py
+++ inventory/chatbot_views.py
@@ -14,7 +14,7 @@ from .models import (
     AseoProducto, PapeleriaProducto
 )
 
-GEMINI_API_KEY = config('AIzaSyBUBSnMucMeL3MX-gCzNqo3QpaAKVgYF1g', default='')
+GEMINI_API_KEY = config('GEMINI_API_KEY', default='')
 GEMINI_URL = (
     "https://generativelanguage.googleapis.com/v1beta/models/"
     "gemini-2.0-flash:generateContent?key=" + GEMINI_API_KEY
